69d4790d048ef2c980b1ab354562f5cfd3144aa4
CDrury0 <cdrury922@gmail.com>
2023-03-10 23:25:00 -0500

    Completed an "Etch-A-Sketch"
    - A grid that gets filled by mousing over each block
    - By default it is 16x16, but can be any size between 1x1 and 100x100 using the new grid button

---
+++ Sketchpad/script.js
@@ -0,0 +1,32 @@
+makeGrid(16);
+
+function newGrid(){
+    let size = prompt("Enter the number of squares per side (max 100)");
+    if(size > 100){
+        size = 100;
+    } else if(size <= 0){
+        size = 1;
+    }
+    const container = document.querySelector("#gridContainer");
+    while(container.hasChildNodes()){
+        container.removeChild(container.firstChild);
+    }
+    makeGrid(size);
+}
+
+function makeGrid(sideLength){
+    const container = document.querySelector("#gridContainer");
+    for(let i = 0; i < sideLength; i++){
+        const columnHead = document.createElement("div");
+        columnHead.className = "head";
+        for(let j = 0; j < sideLength; j++){
+            const gridSquare = document.createElement("div");
+            gridSquare.className = "normal";
+            gridSquare.addEventListener("mouseover", () => {
+                gridSquare.style.backgroundColor = "black";
+            });
+            columnHead.appendChild(gridSquare);
+        }
+        container.appendChild(columnHead);
+    }
+}
